ab5b40be0b003bdd173bf8501f037124a0468a90
Simone Karin Lehmann <simone@lisanet.de>
2025-09-10 21:08:04 +0200

    FXUpscale: use option 'input for input video instead of argmuent, add option 'output'

--- Sources/fx-upscale/FXUpscale.swift
+++ Sources/fx-upscale/FXUpscale.swift
@@ -7,8 +7,11 @@ import Upscaling
 // MARK: - MetalFXUpscale
 
 @main struct FXUpscale: AsyncParsableCommand {
-    @Argument(help: "The video file to upscale", transform: URL.init(fileURLWithPath:)) var url: URL
-
+    @Option(name: [.customShort("i"), .customLong("input")], help: "input video file to upscale",
+                transform: URL.init(fileURLWithPath:))
+            var input: URL
+    @Option(name: [.customShort("o"), .customLong("output")], help: "output video file path")
+    var output: String?
     @Option(name: .shortAndLong, help: "The output file width")
     var width: Int?
     @Option(name: .shortAndLong, help: "The output file height")
@@ -30,15 +33,15 @@ import Upscaling
     var allowFrameReordering: Bool = false
 
     mutating func run() async throws {
-        guard ["mov", "m4v", "mp4"].contains(url.pathExtension.lowercased()) else {
+        guard ["mov", "m4v", "mp4"].contains(input.pathExtension.lowercased()) else {
             throw ValidationError("Unsupported file type. Supported types: mov, m4v, mp4")
         }
 
-        guard FileManager.default.fileExists(atPath: url.path) else {
-            throw ValidationError("File does not exist at \(url.path(percentEncoded: false))")
+        guard FileManager.default.fileExists(atPath: input.path) else {
+            throw ValidationError("File does not exist at \(input.path(percentEncoded: false))")
         }
 
-        let asset = AVAsset(url: url)
+        let asset = AVAsset(url: input)
         guard let videoTrack = try await asset.loadTracks(withMediaType: .video).first else {
             throw ValidationError("Failed to get video track from input file")
         }
@@ -118,7 +121,8 @@ import Upscaling
         let exportSession = UpscalingExportSession(
             asset: asset,
             outputCodec: effectiveOutputCodec,
-            preferredOutputURL: url.renamed { "\($0) Upscaled" },
+            preferredOutputURL: output.map { URL(fileURLWithPath: $0) }
+                                    ?? input.renamed { "\($0) Upscaled" },
             outputSize: outputSize,
             creator: ProcessInfo.processInfo.processName,
             keyframeIntervalSeconds: keyframeInterval,
